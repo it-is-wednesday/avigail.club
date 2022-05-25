22f540f9d54269bd9c022a7fc8f7d051551bf1cc
Maor Kadosh <git@avocadosh.xyz>
2022-05-25 13:57:29 +0300

    replace just with invoke

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-You'll probably want:
+You'll probably need:
+- `invoke`, `jinja2`, and `watchdog` Python libraries
 - [rclone](https://rclone.org/) to sync a drive directory
 - ImageMagick to verify pic size (`identify`) and convert to WEBP (`convert`)
 
---
+++ tasks.py
@@ -0,0 +1,56 @@
+import os
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+from multiprocessing import Process
+
+from watchdog.events import FileSystemEvent, FileSystemEventHandler
+from watchdog.observers import Observer
+
+from generate import main as generate_html
+from invoke import task
+
+
+@task
+def sync(c):
+    c.run('rclone --verbose sync "Nextcloud:/אביגיל" ./static/pics')
+
+
+@task
+def generate(c):
+    generate_html()
+
+
+@task(sync, generate)
+def publish(c):
+    c.run("rsync -Aax --delete ./out/ www-data@avocadosh.xyz:/var/www/avigail/")
+
+
+@task
+def watch(c):
+    def http():
+        # there's no way other way to change the serving directory xd
+        os.chdir("./out/")
+        server_address = ("", 18361)
+        httpd = HTTPServer(server_address, SimpleHTTPRequestHandler)
+        httpd.serve_forever()
+
+    def watch_proc_func():
+        class Handler(FileSystemEventHandler):
+            def on_modified(self, event: FileSystemEvent):
+                if event.is_directory:
+                    return
+                generate(c)
+
+        observer = Observer()
+        observer.schedule(Handler(), "templates", recursive=True)
+        observer.schedule(Handler(), "static", recursive=True)
+        observer.start()
+        observer.join()
+
+    http_proc = Process(target=http)
+    http_proc.start()
+
+    watch_proc = Process(target=watch_proc_func)
+    watch_proc.start()
+
+    http_proc.join()
+    watch_proc.join()
